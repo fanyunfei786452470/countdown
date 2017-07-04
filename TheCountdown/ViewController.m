838b7ced491b8755d74e9e9f14dd6fbb4e250aea
范云飞 <786452470@qq.com>
2017-07-04 10:34:26 +0800

    删除无用的代码

--- TheCountdown/ViewController.m
+++ TheCountdown/ViewController.m
@@ -18,25 +18,16 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
-    //NSString 转 UInt8
-    NSString *str = @"测试转换";
-    uint8_t buff_str[1024];
-    memcpy(buff_str,[str UTF8String], [str length]+1);
-    NSLog(@"char = %s",buff_str);
     
-    NSString *str_From_buff = [NSString stringWithCString:(char*)buff_str encoding:NSUTF8StringEncoding];
-    NSLog(@"string = %@",str_From_buff);
-    
-//    CountDown * button = [[CountDown alloc]init];
-//    [button setTitleColor:[UIColor grayColor] forState:UIControlStateNormal];
-//    [self.view addSubview:button];
-//    self.button = button;
-//    self.button.frame = CGRectMake(100, 100, 150, 30);
-//    [self.button buttonClickWithTimer:5 callBack:^(UIButton *button) {
-//        // 这里发送请求
-//        
-//    }];
-//}
+    CountDown * button = [[CountDown alloc]init];
+    [button setTitleColor:[UIColor grayColor] forState:UIControlStateNormal];
+    [self.view addSubview:button];
+    self.button = button;
+    self.button.frame = CGRectMake(100, 100, 150, 30);
+    [self.button buttonClickWithTimer:5 callBack:^(UIButton *button) {
+        // 这里发送请求
+        
+    }];
 }
 
 - (void)didReceiveMemoryWarning {
